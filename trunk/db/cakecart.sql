1b2bd7ef9369807a89affff98244c82994bddb05
kyle.spearrin <kyle.spearrin@4cea5802-fbfc-11dd-a8e2-f7cb3c35fcff>
2009-02-17 22:06:06 +0000

    db backup

---
+++ trunk/db/cakecart.sql
@@ -0,0 +1,148 @@
+-- phpMyAdmin SQL Dump
+-- version 3.1.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Feb 17, 2009 at 05:05 PM
+-- Server version: 5.1.30
+-- PHP Version: 5.2.8
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `cakecart`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cc_images`
+--
+
+CREATE TABLE IF NOT EXISTS `cc_images` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `filename` varchar(255) CHARACTER SET utf8 NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `cc_images`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cc_images_products`
+--
+
+CREATE TABLE IF NOT EXISTS `cc_images_products` (
+  `image_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `cc_images_products`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cc_products`
+--
+
+CREATE TABLE IF NOT EXISTS `cc_products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `image_id` int(11) NOT NULL,
+  `tax_class_id` int(11) NOT NULL,
+  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `modified` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `title` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `short_description` text CHARACTER SET utf8 NOT NULL,
+  `description` text CHARACTER SET utf8 NOT NULL,
+  `sku` varchar(20) CHARACTER SET utf8 NOT NULL,
+  `new_from` datetime DEFAULT NULL,
+  `new_to` datetime DEFAULT NULL,
+  `price` decimal(12,4) NOT NULL DEFAULT '0.0000',
+  `special_price` decimal(12,4) DEFAULT NULL,
+  `special_price_from` datetime DEFAULT NULL,
+  `special_price_to` datetime DEFAULT NULL,
+  `quantity` int(11) NOT NULL DEFAULT '0',
+  `sort_order` int(11) NOT NULL,
+  `is_stocked` tinyint(1) NOT NULL DEFAULT '1',
+  `is_active` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `cc_products`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cc_products_product_categories`
+--
+
+CREATE TABLE IF NOT EXISTS `cc_products_product_categories` (
+  `product_id` int(11) NOT NULL,
+  `product_category_id` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `cc_products_product_categories`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cc_product_categories`
+--
+
+CREATE TABLE IF NOT EXISTS `cc_product_categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_category_id` int(11) NOT NULL COMMENT 'parent category id',
+  `image_id` int(11) NOT NULL,
+  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `modified` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `name` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `description` text CHARACTER SET utf8 NOT NULL,
+  `sort_order` tinyint(1) NOT NULL,
+  `is_active` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `cc_product_categories`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cc_tax_classes`
+--
+
+CREATE TABLE IF NOT EXISTS `cc_tax_classes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `modified` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `title` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `description` varchar(255) CHARACTER SET utf8 NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `cc_tax_classes`
+--
+
