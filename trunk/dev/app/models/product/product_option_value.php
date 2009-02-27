6533ec3c6a361520b1fc339afad2b38a698c8f6c
kyle.spearrin <kyle.spearrin@4cea5802-fbfc-11dd-a8e2-f7cb3c35fcff>
2009-02-27 07:19:19 +0000

    Improved SQL queries for avg_ratings on categories.
    Product option display added.

--- trunk/dev/app/models/product/product_option_value.php
+++ trunk/dev/app/models/product/product_option_value.php
@@ -2,6 +2,7 @@
 class ProductOptionValue extends AppModel {
 
 	var $name = 'ProductOptionValue';
+	var $displayField = 'value';
 
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
 	var $belongsTo = array(
